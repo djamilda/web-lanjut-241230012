ca0c7c27b8b69b59280df97eb7007adc4cf4dec0
putri <putput@gmail.com>
2026-04-26 14:47:40 +0700

    tambah view kontak

--- resources/views/resources/views/kontak.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <! -- the whole future lies in uncertainty: live immediately, - seneca -->
-    <a href="/">Home</a>
-    <a href="/kontak">kontak</a>
-    <a href="/profil">profil</a>
-</div>
